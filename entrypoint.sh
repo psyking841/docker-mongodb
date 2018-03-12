97c04c61b1f212b1a97f74cf609e9d3e9c33b82f
Shengyi Pan <shengyipan@Shengyis-MacBook-Pro.local>
2018-03-12 15:16:58 -0400

    enable authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,6 @@ USER_ID=${LOCAL_USER_ID:-502}
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m shengyipan
-export HOME=/home/shengyipan
 
 # exec /usr/local/bin/gosu shengyipan "$@"
-exec /usr/local/bin/gosu shengyipan mongod --dbpath /data/db/
+exec /usr/local/bin/gosu shengyipan mongod --auth --dbpath /data/db/ --bind_ip 0.0.0.0 --port 27017
